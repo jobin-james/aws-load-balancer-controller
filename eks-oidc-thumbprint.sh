0ed1aa677b4bad8a4780bbd7a2ddd6d611ebf5b2
Jobin James <jobinjames47@gmail.com>
2021-02-12 14:28:51 +0530

    Delete eks-oidc-thumbprint.sh

--- eks-oidc-thumbprint.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-EKS_URL="oidc.eks.${1}.amazonaws.com"
-THUMBPRINT=$(openssl s_client -servername ${EKS_URL} -showcerts -connect ${EKS_URL}:443 </dev/null 2>&1 \
-    | openssl x509 -fingerprint -noout | awk -F= '{print tolower($2)}' | sed 'ss:ssg')
-echo -en "{\"value\": \"${THUMBPRINT}\"}"
